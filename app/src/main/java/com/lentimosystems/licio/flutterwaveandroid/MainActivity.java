a562dc560cc3dd7b976c1794e213db7bb737cdd1
Naphtali <naphtali919@gmail.com>
2022-04-12 20:36:44 +0300

    Added first implementation

---
+++ app/src/main/java/com/lentimosystems/licio/flutterwaveandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.lentimosystems.licio.flutterwaveandroid;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.flutterwave.raveandroid.RaveConstants;
+import com.flutterwave.raveandroid.RavePayActivity;
+import com.flutterwave.raveandroid.RavePayManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
