e5423e9246545f66395e3b18bde8da2c8013b732
Naphtali <naphtali919@gmail.com>
2022-04-12 21:06:48 +0300

    Implemented Mpesa and CArd

--- app/src/main/java/com/lentimosystems/licio/flutterwaveandroid/MainActivity.java
+++ app/src/main/java/com/lentimosystems/licio/flutterwaveandroid/MainActivity.java
@@ -20,5 +20,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pay = findViewById(R.id.pay);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makePayment();
+            }
+        });
+    }
+
+    private void makePayment() {
+        new RavePayManager(this)
+                .setAmount(Double.parseDouble("1"))
+                .setEmail("naphtali919@gmail.com")
+                .setCountry("KE")
+                .setCurrency("KES")
+                .setfName("Licio")
+                .setlName("Lentimo")
+                .setNarration("Purchase Goods")
+                .setPublicKey("FLWPUBK_TEST-73803d8673513aa0bbdd399d628a7039-X")
+                .setEncryptionKey("FLWSECK_TEST8245011a68e2")
+                .setTxRef(System.currentTimeMillis() + "Ref")
+                .acceptAccountPayments(true)
+                .acceptCardPayments(true)
+                .acceptMpesaPayments(true)
+                .onStagingEnv(true)
+                .shouldDisplayFee(true)
+                .showStagingLabel(true)
+                .initialize();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RaveConstants.RAVE_REQUEST_CODE && data != null) {
+            String message = data.getStringExtra("response");
+            if (resultCode == RavePayActivity.RESULT_SUCCESS) {
+                Toast.makeText(this, "SUCCESS " + message, Toast.LENGTH_LONG).show();
+            }
+            else if (resultCode == RavePayActivity.RESULT_ERROR) {
+                Toast.makeText(this, "ERROR " + message, Toast.LENGTH_LONG).show();
+            }
+            else if (resultCode == RavePayActivity.RESULT_CANCELLED) {
+                Toast.makeText(this, "CANCELLED " + message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
